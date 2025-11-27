8c6d463d7842541cb61a337660cc3c6d0d665ef3
Nelo-T. Wallus <n.wallus@sap.com>
2025-11-28 00:36:10 +0100

    Fix rewriting kcp-admin instead of kco-from-host kubeconfig
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- hack/lib.bash
+++ hack/lib.bash
@@ -328,13 +328,13 @@ kcp::setup::kubeconfigs() {
         > "$kcp_kubeconfig" \
         || die "Failed to get admin kubeconfig from kind cluster"
 
-    # Replace the port with the node port from the service
-    yq -i ".clusters[].cluster.server |= sub(\":32443\"; \":8443\")" "$kcp_kubeconfig"
+    # Replace the port with the port-forwarded port
 
     # Create port forward to access kcp from host
     kcp::front_proxy_forward "$kind_kubeconfig" "8443"
-    # kcp::root_shard_forward "$kind_kubeconfig" "8443"
+
     cp "$kcp_kubeconfig" "$kcp_host_kubeconfig"
+    yq -i ".clusters[].cluster.server |= sub(\":32443\"; \":8443\")" "$kcp_host_kubeconfig"
     local hostname="$(kubectl --kubeconfig "$kind_kubeconfig" get rootshards.operator.kcp.io root -o jsonpath='{.spec.external.hostname}')"
     kubeconfig::hostname::set "$kcp_host_kubeconfig" "$hostname:32443" "127.0.0.1:8443"
 }
